c3b723837ab7a3eed1ff9ed8bb19f0f6080cfaf8
Edward Kim <edwardkim@Edwards-MacBook-Pro.local>
2023-07-04 22:20:35 -1000

    Added ability to display numbers pressed to display box

--- scripts.js
+++ scripts.js
@@ -1,3 +1,28 @@
+const displayBox = document.querySelector(".display-box");
+const digitButton = document.querySelectorAll("#digit-button");
+const clearButton = document.querySelector("#clear-button");
+const allClearButton = document.querySelector("#all-clear-button");
+const operatorButton = document.querySelector("#operator");
+const equalButton = document.querySelector("equal-button");
+const decimalButton = document.querySelector("decimal-button"); //could add this into the digitButton
+
+//let memory = [];
+
+//Event listener for digit buttons and call display
+//to display the string of number on display box
+digitButton.forEach((number) =>
+  number.addEventListener("click", () => {
+    display(number.textContent);
+  })
+);
+
+//display function to display in display box of calculator
+function display(val) {
+  displayBox.textContent += val;
+  //console.log(val);
+}
+
+//The basic functions of a calculator
 const subtract = function (a, b) {
   return a - b;
 };
